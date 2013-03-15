f3f0c20278c4b672d8205061fb783f33bf16156d
Andrea <andrea@campo23.(none)>
2013-03-15 23:24:20 +0100

    added move function

--- Ship.py
+++ Ship.py
@@ -5,7 +5,16 @@ class Ship:
 	def __init__(self, screen, h, w):
 		self.width = 50
 		self.height = 50
-		self.ship = pygame.Rect(w/2, h-50, self.width, self.height)
+		self.x = h/2
+		self.y = w-50		
+		self.shipR = pygame.Rect(self.x, self.y, self.width, self.height)
 
 	def draw(self, screen):
-		pygame.draw.rect(screen, (0, 255, 0), self.ship)
+		pygame.draw.rect(screen, (0, 255, 0), self.shipR)
+
+	def move(self, key, h):
+		if(key[pygame.K_d] and self.x<h-50):
+			self.x +=2
+		elif(key[pygame.K_a] and self.x>0):
+			self.x -=2
+		self.shipR = pygame.Rect(self.x, self.y, self.width, self.height)
--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from Ship import Ship
 def run():
 	pygame.init()   
 	start = 1
-	h = 800
+	h = 1024
 	w = 600
 	screen = pygame.display.set_mode((h, w))
 	ship = Ship(screen, h, w)
@@ -13,9 +13,9 @@ def run():
 		event = pygame.event.poll()
 		if event.type == pygame.QUIT:
 			running = 0
-		print "2"
-		screen.fill((0, 0, 0)) 
-		print "hello"
+		screen.fill((0, 0, 0))
+		key = pygame.key.get_pressed()
+		ship.move(key, h)
 		ship.draw(screen)
 		pygame.display.flip()
 
